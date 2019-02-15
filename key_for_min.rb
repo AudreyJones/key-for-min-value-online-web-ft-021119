8525de8b16f0e512b2dcb59c058f035684167f43
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 19:40:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,13 @@ require "pry"
 # {:blake=>500, :ashley=>2, :adam=>1}
 def key_for_min_value(name_hash)
   min_value = nil
+  min_key = nil
   name_hash.each do |key,value|
     # Have we set anything to the min value? If yes, compare to other numbers.
     if min_value == nil || value < min_value
       min_value = value
     end
-    name_hash[key]
+    
   end
 
 end
