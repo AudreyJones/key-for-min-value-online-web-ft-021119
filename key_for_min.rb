4b0b6cdff4a649d89c48555c38f92884f26ba6f7
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 18:58:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,10 @@ require "pry"
 
 def key_for_min_value(name_hash)
   name_hash.each do |key,value|
-    value.each do |a, b|
+    value.collect do |a, b|
       a <=> b
 binding.pry
     end
   end
+  
 end
