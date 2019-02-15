84db4e4d2a033aed3f5eff33432a4e4c9093ef77
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-15 19:47:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |key,value|
     # Have we set anything to the min value? If yes, compare to other numbers.
-    if min_value == nil || value < min_value
+    if  value < min_value
       min_value = value
       min_key = key
     end
